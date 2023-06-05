2bfb927953103320e91537ef428b172539b82d25
Cas Brugman <casbrugman@hotmail.nl>
2023-06-05 20:08:13 +0200

    Update damper.rs

--- src/damper.rs
+++ src/damper.rs
@@ -13,6 +13,11 @@ impl<T> LinearDamper<T> {
 }
 
 impl LinearDamper<f32> {
+    ///Creates a damper that can be used to stop some kind of wave on the basis that humans can't hear waves under 20Hz
+    pub fn cutoff(sample_rate: u32) -> Self {
+        Self::new(1.0 / (sample_rate as f32 / 20.0), 0.0)
+    }
+
     pub fn frame(&mut self, input: f32) -> f32 {
         let dif = (input - self.current).clamp(-self.max_dif, self.max_dif);
         self.current += dif;
